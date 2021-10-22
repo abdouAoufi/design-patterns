188a603f1c47419ae467eaeb9a8b54d71a453a59
AbdouAoufi <Abdouou7@gmail.com>
2021-10-22 11:09:31 +0100

    Add Proxy design pattern

---
+++ 11 - PROXY/README.md
@@ -0,0 +1,10 @@
+### PROXY DESIGN PATTER
+___
+
+In proxy pattern, a class represents functionality of another class. This type of design pattern comes under structural pattern.
+
+In proxy pattern, we create object having original object to interface its functionality to outer world.
+
+![](https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.softwareideas.net%2Fi%2FDirectImage%2F432%2FProxy-design-pattern.png&f=1&nofb=1)
+
+#### Implementation in JavaScript
---
+++ 11 - PROXY/proxy.js
@@ -0,0 +1,48 @@
+class Image {
+    constructor() {
+        if (this.constructor.name === "Image") {
+            throw Error("This is abstract class")
+        }
+    }
+    display() { }
+}
+
+
+class RealImage extends Image {
+    constructor(fileName) {
+        super();
+        if (!fileName) throw Error("No filename provided")
+        this.fileName = fileName;
+    }
+
+    display() {
+        console.log("Displaying " + this.fileName)
+    }
+
+    laodFromDisk() {
+        console.log("Loading ", this.fileName)
+    }
+}
+
+class ImageProxy extends Image {
+    realImage;
+    constructor(fileName) {
+        super();
+        if (!fileName) throw Error("No filename provided")
+        this.fileName = fileName;
+    }
+    display() {
+        if (!this.realImage) {
+            this.realImage = new RealImage(this.fileName);
+        }
+        this.realImage.display();
+    }
+}
+
+
+const Demo = (() => {
+    const fileName = "abdou/images/11.png";
+    const imageProxy = new ImageProxy(fileName);
+    imageProxy.display();
+})();
+
